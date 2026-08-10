d3f23844d25090ff61b3712c66daaf673edf725c
umargithub <md.umar102@gmail.com>
2026-08-10 19:46:01 +0000

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata = {
+  title: "Nestwell - Find your home",
+  description: "Find the perfect rental property in your town!",
+  keywords: "rental, property, real estate",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${poppins.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        {children}
+      </body>
+    </html>
+  );
+}
