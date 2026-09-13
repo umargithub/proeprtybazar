3f491200fab2ecccaf646beb31ad6eb514eacfad
umargithub <md.umar102@gmail.com>
2026-09-13 10:34:50 +0000

    Fixed property coomponent bug

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -57,12 +57,21 @@ export default function PropertyCard({ property }) {
         </div>
 
         <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
-          <p>
-            <FaMoneyBill className="md:hidden lg:inline" /> Weekly
-          </p>
-          <p>
-            <FaMoneyBill className="md:hidden lg:inline" /> Monthly
-          </p>
+          {property.rates.nightly && (
+            <p>
+              <FaMoneyBill className="inline-block mr-2" /> Nightly
+            </p>
+          )}
+          {property.rates.weekly && (
+            <p>
+              <FaMoneyBill className="inline-block mr-2" /> Weekly
+            </p>
+          )}
+          {property.rates.monthly && (
+            <p>
+              <FaMoneyBill className="inline-block mr-2" /> Monthly
+            </p>
+          )}
         </div>
 
         <div className="border border-gray-100 mb-5"></div>
